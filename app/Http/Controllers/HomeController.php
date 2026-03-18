81e8205a6f5278dda93b57b1ab93a7b45d71445a
Zarko Gavric <zarko.gavric@sportserve.co>
2026-03-18 20:53:46 +0100

    Integrate Flowbite for UI enhancements, implement API response caching, and update view routes.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,27 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use EinarHansen\FootballData\FootballDataService;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function showAllCompetitions(FootballDataService $footballDataService)
     {
-        $allCompetitions = $footballDataService->competitions()->all();
-
-        return view("all-competitions", compact("allCompetitions"));
+        $allCompetitions = Cache::remember('all_competitions', 86400, function () use ($footballDataService) {
+            return $footballDataService->competitions()->all();
+        });
+        return view('all-competitions', compact('allCompetitions'));
     }
 
     public function showCompetition(FootballDataService $footballDataService, $competitionId) {
-        $competition = $footballDataService->competitions()->teams($competitionId);
-
-        return view("competition", compact("competition"));
+        $cacheKey = "competition_{$competitionId}";
+        
+        $competition = Cache::remember($cacheKey, 86400, function () use ($footballDataService, $competitionId) {
+            return $footballDataService->competitions()->teams($competitionId);
+        });
+        return view('competition', compact('competition'));
     }
 
     public function showTeam(FootballDataService $footballDataService, $teamId) {
-        $team = $footballDataService->teams()->find($teamId);
-
+        $cacheKey = "team_{$teamId}";
+        $team = Cache::remember($cacheKey, 86400, function () use ($footballDataService, $teamId) {
+            return $footballDataService->teams()->find($teamId);
+        });
         return view('football-club', compact('team'));
     }
 }
